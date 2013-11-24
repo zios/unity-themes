5a6ae76127799154a129593ba4faab5f87b4ce78
BradSmithee <BradSmithee@mycompany.com>
2013-11-24 20:41:37 +0000

    -- Extended BitStream to accept objects (with or without specified type). -- Renamed SnapshotManager to SnapshotController -- Wrapped SnapshotController inside a Namespace to reduce long names and keep it relatively encapsulated.
    git-svn-id: http://ziosproject.com/Shared@3 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

---
+++ Codebase/Components/Editor/SnapshotControllerEditor.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using Controller = Zios.Snapshot.SnapshotController;
+namespace Zios.Snapshot{
+	[CustomEditor(typeof(Controller))]
+	public class SnapshotControllerEditor : Editor{
+		private CustomAddElement addElement;
+		public override void OnInspectorGUI(){
+			if(this.addElement == null){
+				this.addElement = new CustomAddElement("",this.target,new CustomListElement(target));
+			}
+			this.addElement.Draw();
+			if(this.addElement.list.shouldRepaint){
+				this.Repaint();
+			}
+			if(GUI.changed){
+				EditorUtility.SetDirty(target);
+			}
+		}
+		class RemoveItemAction : ListAction{
+			public override void OnAction(UnityEngine.Object target,object targetItem){
+				float width = 70f;
+				if(GUILayout.Button("Remove",GUILayout.Width(width))){
+					((Controller)target).Remove((Configuration)targetItem);
+				}
+			}
+		}
+		class CustomListElement : ListElementsTemplate{
+			public CustomListElement(UnityEngine.Object target):base(target){
+			}
+			public override void CreateActions(){
+				this.actions.Add(new RemoveItemAction());
+			}
+			public override void CreateItems(){
+				float width = 100f;
+				this.listItems.Add(new ListItem("Component","componentName",width,ItemTypes.Label));
+				this.listItems.Add(new ListItem("Attribute","attributeName",width,ItemTypes.Label));
+				this.listItems.Add(new ListItem("Type","type",width,ItemTypes.Label));
+				this.listItems.Add(new ListItem("Send Type","sendType",width,ItemTypes.Enumeration));
+			}
+			public override List<object> GetList(){
+				List<object> elements = new List<object>();
+				foreach(Configuration configuration in ((Controller)this.target).configurations){
+					elements.Add(configuration);
+				}
+				return elements;
+			}
+		}
+		class ComponentsSelectbox : Selectbox{
+			public Controller target;
+			public ComponentsSelectbox(float width,Controller target):base(width){
+				this.target = target;
+			}
+			public override void OnClick(){
+				this.target.LoadComponents();
+				base.OnClick();
+			}
+		}
+		class CustomAddElement : AddElementTemplate{
+			public CustomAddElement(string title,UnityEngine.Object target,CustomListElement list):base(title,target,list){
+			}
+			public override void CreateSelectboxes(){
+				float width = 100f;
+				this.selectboxes.Add(new ComponentsSelectbox(width,(Controller)this.target));
+				this.selectboxes.Add(new Selectbox(width));
+			}
+			public override void UpdateSelectboxes(){
+				Controller controller = (Controller)this.target;
+				if(controller.components.Count == 0){
+					controller.LoadComponents();
+				}
+				Selectbox componentsBox = this.selectboxes[0];
+				Selectbox attributesBox = this.selectboxes[1];
+				if(componentsBox.Changed()){
+					string[] componentsNames = new string[controller.components.Count];
+					foreach(Component component in controller.components){
+						componentsNames[controller.components.IndexOf(component)] = component.GetType().Name;
+					}
+					componentsBox.options = componentsNames;
+					controller.SelectComponent(componentsBox.index);
+					attributesBox.options = controller.attributes.ToArray();
+					if(attributesBox.index > attributesBox.options.Length){
+						attributesBox.index = 0;
+					}
+				}
+			}
+			public override void AddElement(){
+				Selectbox componentsBox = this.selectboxes[0];
+				Selectbox attributesBox = this.selectboxes[1];
+				Controller controller = (Controller)this.target;
+				controller.Add(componentsBox.index,attributesBox.index);
+			}
+		}
+	}
+}
---
+++ Codebase/Components/SnapshotController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Zios.Snapshot{
+	[ExecuteInEditMode]
+	[AddComponentMenu("Zios/Component/General/Snapshot Controller")]
+	public class SnapshotController : MonoBehaviour{
+		public List<Configuration> configurations = new List<Configuration>();
+		public List<Component> components = new List<Component>();
+		public List<string> attributes = new List<string>();
+		private List<string> skipAttributes = new List<string>();
+		private Component currentComponent;
+		void Awake(){
+			this.skipAttributes = this.ListAttributes();
+		}
+		public void LoadComponents(){
+			this.components = new List<Component>();
+			Component[] allComponents = GetComponents<Component>();
+			foreach(Component component in allComponents){
+				if(!component.GetType().Equals(this.GetType())){
+					this.components.Add(component);
+				}
+			}
+		}
+		public void SelectComponent(int index){
+			this.currentComponent = components[index];
+			this.attributes = new List<string>();
+			List<string> allAttributes = this.currentComponent.ListAttributes();
+			foreach(string attribute in allAttributes){
+				if(!this.skipAttributes.Contains(attribute)){
+					this.attributes.Add(attribute);
+				}
+			}
+		}
+		public void Remove(Configuration setting){
+			this.configurations.Remove(setting);
+		}
+		public void Add(int componentIndex,int attributeIndex){
+			Component component = this.components[componentIndex];
+			string attributeName = this.attributes[attributeIndex];
+			foreach(Configuration configuration in this.configurations){
+				if(configuration.component.Equals(component) && configuration.attributeName.Equals(attributeName)){
+					return;
+				}
+			}
+			this.configurations.Add(new Configuration(component,attributeName));
+		}
+		public void OnSerializeNetworkView(BitStream stream,NetworkMessageInfo info){
+			foreach(Configuration configuration in this.configurations){
+				if(configuration.accessor == null){
+					configuration.accessor = new Accessor(configuration.component,configuration.attributeName);
+				}
+				object value = configuration.accessor.Get();
+				stream.Serialize(ref value);
+				if(stream.isWriting){configuration.accessor.Set(value);}
+			}
+		}
+	}
+	[Serializable]
+	public class Configuration{
+		public Component component;
+		public string componentName;
+		public string attributeName;
+		public string type;
+		public SerializeType sendType;
+		public Accessor accessor;
+		public Configuration(Component component,string attributeName){
+			this.component = component;
+			this.componentName = this.component.GetType().Name;
+			this.attributeName = attributeName;
+			this.accessor = new Accessor(this.component,this.attributeName);
+			this.type = this.accessor.Get().GetType().Name;
+			this.sendType = SerializeType.Int;
+			foreach(SerializeType type in Enum.GetValues(typeof(SerializeType))){
+				if(type.ToString().ToLower().Equals(this.type.ToLower())){
+					this.sendType = type;
+					break;
+				}
+			}
+		}
+	}
+	public enum SerializeType{Bool,Char,Short,Int,Float,Quaternion,Vector3};
+}
