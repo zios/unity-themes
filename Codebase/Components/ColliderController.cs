2dcde486a84de74d9d150dc70f7a9b1850d2c8f5
BrunoLima <BrunoLima@mycompany.com>
2014-01-12 13:44:07 +0000

    ColliderController - Checking for collisions using a Rigidbody hack. Working for all colliders.
    git-svn-id: http://ziosproject.com/Shared@15 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

--- Codebase/Components/ColliderController.cs
+++ Codebase/Components/ColliderController.cs
@@ -5,48 +5,33 @@ using System;
 public class ColliderController : MonoBehaviour{
 	[HideInInspector]
 	public Vector3 move = Vector3.zero;
-	void Update(){
+	public void OnEnable(){
+		if(this.rigidbody == null){
+			this.gameObject.AddComponent("Rigidbody");
+		}
+		this.rigidbody.Sleep();
+	}
+	void FixedUpdate(){
 		if(this.move != Vector3.zero){
+			this.rigidbody.WakeUp();
 			this.move *= Time.deltaTime;
 			RaycastHit hit = new RaycastHit();
 			Vector3 start = this.transform.position;
 			Vector3 end = start + this.move;
 			float distance = Vector3.Distance(start,end);
-			this.collider.enabled = false;
-			if(this.collider is SphereCollider){
-				float radius = ((SphereCollider)this.collider).radius;
-				if(Physics.SphereCast(start,radius,this.move.normalized,out hit,distance)){
-					if(hit.distance > 0){
-						this.move = this.move.normalized * hit.distance;
-					}
-					else{
-						this.move = Vector3.zero;
-					}
+			if(this.rigidbody.SweepTest(this.move.normalized,out hit,distance)){
+				if(hit.distance > 0){
+					this.move = this.move.normalized * hit.distance;
+				}
+				else{
+					this.move = Vector3.zero;
 				}
 			}
-			else if(this.collider is CapsuleCollider){
-				float height = ((CapsuleCollider)this.collider).height;
-				float radius = ((CapsuleCollider)this.collider).radius;
-				Vector3 center = ((CapsuleCollider)this.collider).center;
-				Vector3 rotation = this.transform.rotation * Vector3.up;
-				Vector3 p1 = this.transform.position + center + rotation * (-height * 0.5F + radius);
-				Vector3 p2 = this.transform.position + center + rotation * (height * 0.5F - radius);
-				if(Physics.CapsuleCast(p1,p2,radius,this.move.normalized,out hit,distance)){
-					if(hit.distance > 0){
-						this.move = this.move.normalized * hit.distance;
-					}
-					else{
-						this.move = Vector3.zero;
-					}
-				}
-			}
-			else if(this.collider is BoxCollider){
-			}
-			this.collider.enabled = true;
 			if(this.move != Vector3.zero){
 				this.transform.position += this.move;
 				this.move = Vector3.zero;
 			}
+			this.rigidbody.Sleep();
 		}
 	}
 	public void Move(Vector3 move){
