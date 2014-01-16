a6c3ffe1818d9bb07cff9ac359786c44d51c7405
BradSmithee <BradSmithee@mycompany.com>
2014-01-16 07:47:37 +0000

    -- Added support for Trigger Colliders in ColliderController. -- Tightened behaviour of skin and hover offsets.
    git-svn-id: http://ziosproject.com/Shared@24 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

--- Codebase/Components/ColliderController.cs
+++ Codebase/Components/ColliderController.cs
@@ -18,14 +18,33 @@ public class CollisionData{
 }
 [AddComponentMenu("Zios/Component/General/Collider Controller")]
 public class ColliderController : MonoBehaviour{
+	static public Collider[] triggers;
+	static public bool triggerSetup;
 	public List<Vector3> move = new List<Vector3>();
 	[HideInInspector] public Dictionary<string,bool> blocked = new Dictionary<string,bool>();
-	public float hoverWidth = 0.05f;
-	public float skinWidth = 0.05f;
+	public float hoverWidth = 0.0001f;
+	public float skinWidth = 0.0001f;
 	public bool persistentBlockChecks;
+	static public void Setup(){
+		if(!ColliderController.triggerSetup){
+			Collider[] colliders = (Collider[])Resources.FindObjectsOfTypeAll(typeof(Collider));
+			List<Collider> triggers = new List<Collider>();
+			foreach(Collider collider in colliders){
+				if(collider.isTrigger){
+					collider.isTrigger = false;
+					triggers.Add(collider);
+				}
+			}
+			ColliderController.triggers = triggers.ToArray();
+			ColliderController.triggerSetup = true;
+		}
+	}
 	public void Awake(){
 		Events.Add("OnMove",this.Move);
 		this.ResetBlocked();
+	}
+	public void Start(){
+		ColliderController.Setup();
 	}
 	public void OnEnable(){
 		if(this.rigidbody == null){
@@ -68,12 +87,11 @@ public class ColliderController : MonoBehaviour{
 			this.ResetBlocked();
 			this.rigidbody.WakeUp();
 			foreach(Vector3 current in this.move){
-				RaycastHit hit;
 				Vector3 move = current * Time.deltaTime;
 				Vector3 direction = move.normalized;
-				if(this.rigidbody.SweepTest(direction,out hit,move.magnitude)){
-					float hitDistance = Mathf.Clamp(hit.distance,this.skinWidth,1);
-					move = direction * (hitDistance - this.skinWidth);
+				RaycastHit hit;
+				if(this.rigidbody.SweepTest(direction,out hit,move.magnitude+this.hoverWidth)){
+					move = direction * (hit.distance - this.skinWidth - this.hoverWidth);
 					CollisionData otherCollision = new CollisionData(this,this.gameObject,-direction,move.magnitude,false);
 					CollisionData selfCollision = new CollisionData(this,hit.transform.gameObject,direction,move.magnitude,true);
 					if(direction.z > 0){this.blocked["forward"] = true;}
@@ -82,6 +100,11 @@ public class ColliderController : MonoBehaviour{
 					if(direction.y < 0){this.blocked["down"] = true;}
 					if(direction.x > 0){this.blocked["right"] = true;}
 					if(direction.x < 0){this.blocked["left"] = true;}
+					bool isTrigger = Array.IndexOf(ColliderController.triggers,hit.collider) != -1;
+					if(isTrigger){
+						hit.transform.gameObject.Call("OnTrigger",this.collider);
+						continue;
+					}
 					hit.transform.gameObject.Call("OnCollide",otherCollision);
 					this.gameObject.Call("OnCollide",selfCollision);
 				}
