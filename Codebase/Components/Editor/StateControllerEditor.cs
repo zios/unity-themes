bedd1484c1436e34de13d7afb857695557247361
BrunoLima <BrunoLima@mycompany.com>
2014-01-27 01:19:43 +0000

    StateController - Editor for the StateController.
    git-svn-id: http://ziosproject.com/Shared@49 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

---
+++ Codebase/Components/Editor/StateControllerEditor.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+[CustomEditor(typeof(StateController))]
+public class StateControllerEditor : Editor{
+	private CustomTableElement tableElement;
+	public override void OnInspectorGUI(){
+		if(this.tableElement == null){
+			this.tableElement = new CustomTableElement(target);
+		}
+		this.tableElement.Draw();
+		if(this.tableElement.shouldRepaint){
+			this.Repaint();
+		}
+		if(GUI.changed){
+			EditorUtility.SetDirty(target);
+		}
+	}
+	public class CustomTableRow : TableRow{
+		public CustomTableRow(string label,bool allowNegative,object target):base(label,allowNegative,target){
+		}
+		public override void PopulateChecks(){
+			StateTable stateTable = (StateTable)this.target;
+			foreach(StateRequirement requirement in stateTable.requirements){
+				if(requirement.requireOn){
+					this.positiveChecks.Add(requirement.name);
+				}
+				else if(requirement.requireOff){
+					this.negativeChecks.Add(requirement.name);
+				}
+			}
+		}
+		public override void Toogle(string state){
+			StateTable stateTable = (StateTable)this.target;
+			foreach(StateRequirement requirement in stateTable.requirements){
+				if(requirement.name.Equals(state)){
+					if(requirement.requireOn){
+						requirement.requireOn = false;
+						if(this.allowNegative){
+							requirement.requireOff = true;
+						}
+					}
+					else if(requirement.requireOff){
+						requirement.requireOff = false;
+					}
+					else{
+						requirement.requireOn = true;
+						requirement.requireOff = false;
+					}
+				}
+			}
+		}
+		public override void CheckContext(){
+			GenericMenu menu = new GenericMenu();
+			StateTable stateTable = (StateTable)this.target;
+			string label = "End if unusable";
+			if(stateTable.endIfUnusable){
+				label += " ✓";
+			}
+			GUIContent field = new GUIContent(label);
+			menu.AddItem(field,false,new GenericMenu.MenuFunction(ChangeEndIfUnusable));
+			menu.ShowAsContext();
+			Event.current.Use();
+		}
+		public void ChangeEndIfUnusable(){
+			StateTable stateTable = (StateTable)this.target;
+			stateTable.endIfUnusable = stateTable.endIfUnusable == false;
+		}
+	}
+	public class CustomTableElement : TableTemplate{
+		public CustomTableElement(UnityEngine.Object target):base(target){
+		}
+		public override void CreateHeaders(){
+			this.headers.Add(string.Empty);
+			foreach(StateTable table in ((StateController)target).data){
+				this.headers.Add(table.name);
+				if(table.name.Length > this.labelSize){
+					this.labelSize = table.name.Length;
+				}
+			}
+		}
+		public override void CreateItems(){
+			this.tableItems = new List<TableRow>();
+			foreach(StateTable stateTable in ((StateController)target).data){
+				this.tableItems.Add(new CustomTableRow(stateTable.name,true,stateTable));
+			}
+		}
+	}
+}
---
+++ Codebase/Components/Editor/Templates/TableElement.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+public abstract class TableTemplate{
+	public UnityEngine.Object target;
+	public List<TableRow> tableItems;
+	public List<string> headers;
+	public bool shouldRepaint;
+	public float labelSize = 0;
+	public float labelWidth;
+	public GUIStyle customStyle = new GUIStyle();
+	public TableTemplate(UnityEngine.Object target){
+		this.target = target;
+		this.headers = new List<string>();
+		this.tableItems = new List<TableRow>();
+		this.CreateHeaders();
+		this.customStyle.wordWrap = true;
+		this.customStyle.fixedWidth = 1;
+		this.customStyle.alignment = TextAnchor.LowerRight;
+		this.customStyle.fixedHeight = this.labelSize * 13;
+		this.labelWidth = this.labelSize * 8;
+	}
+	public void Draw(){
+		this.shouldRepaint = false;
+		this.CreateItems();
+		EditorGUILayout.BeginVertical();
+		EditorGUILayout.BeginHorizontal();
+		foreach(string header in this.headers){
+			this.CreateHeader(header);
+		}
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.EndVertical();
+		EditorGUILayout.BeginVertical();
+		foreach(TableRow item in this.tableItems){
+			EditorGUILayout.BeginHorizontal();
+			item.Draw(headers,this.labelWidth);
+			if(item.shouldRepaint){
+				this.shouldRepaint = true;
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+		EditorGUILayout.EndVertical();
+		if(GUI.changed){
+			EditorUtility.SetDirty(target);
+		}
+	}
+	private void CreateHeader(string header){
+		if(string.IsNullOrEmpty(header)){
+			EditorGUILayout.LabelField(new GUIContent(header),GUILayout.Width(this.labelWidth));
+		}
+		else{
+			EditorGUILayout.LabelField(new GUIContent(header),this.customStyle);
+		}
+	}
+	public abstract void CreateHeaders();
+	public abstract void CreateItems();
+}
+public abstract class TableRow{
+	public string label;
+	public List<string> positiveChecks;
+	public List<string> negativeChecks;
+	public object target;
+	public bool allowNegative;
+	public bool shouldRepaint;
+	public TableRow(string label,bool allowNegative,object target){
+		this.label = label;
+		this.positiveChecks = new List<string>();
+		this.negativeChecks = new List<string>(); 
+		this.allowNegative = allowNegative;
+		this.target = target;
+		this.PopulateChecks();
+	}
+	public void Draw(List<string> headers,float labelWidth){
+		EditorGUILayout.LabelField(new GUIContent(label),GUILayout.Width(labelWidth - 11));
+		if(GUILayoutUtility.GetLastRect().Contains(Event.current.mousePosition) && Event.current.type == EventType.ContextClick){
+			this.CheckContext();
+			this.shouldRepaint = true;
+		}
+		foreach(string state in headers){
+			if(!string.IsNullOrEmpty(state)){
+				string symbol = " ";
+				if(positiveChecks.Contains(state)){
+					symbol = "✓";
+				}
+				else if(negativeChecks.Contains(state)){
+					symbol = "x";
+				}
+				if(GUILayout.Button(new GUIContent(symbol),GUILayout.Width(24))){
+					this.Toogle(state);
+					this.shouldRepaint = true;
+				}
+			}
+		}
+	}
+	public abstract void PopulateChecks();
+	public abstract void Toogle(string state);
+	public abstract void CheckContext();
+}
