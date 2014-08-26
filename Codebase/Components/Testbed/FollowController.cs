5651e2b99ae3d4cb6222b2bc0c79566efd53cddd
ArnoldTakacs <ArnoldTakacs@mycompany.com>
2014-08-26 10:54:15 +0000

    These scripts are there for testing out and assisting the ColliderController collision detection against mesh colliders: - FollowController is being used to assist the Follow Component of the camera in use - MoveTest script is there to assist the ColliderController's vector3 movement
    git-svn-id: http://ziosproject.com/Shared@92 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

---
+++ Codebase/Components/Testbed/FollowController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+public class FollowController : MonoBehaviour{
+	public GameObject player;
+	public KeyCode playerTurnKey = KeyCode.W;
+	public KeyCode playerTurnKeyOptional = KeyCode.S;
+	public float x,y,z,xSpeed,ySpeed,xMax,xMin,yMax,yMin;
+	public Follow follow;
+	void Start(){
+		follow=gameObject.GetComponent<Follow>();}
+	void Update(){
+		x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+		y += Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+		z += Input.GetAxis("Mouse ScrollWheel");
+		y = Mathf.Clamp (y,yMin,yMax);
+		follow.orbitAngles.x = x;
+		follow.orbitAngles.y = y;
+		follow.targetOffset.z = z;
+		if(Input.GetKey(playerTurnKey) || Input.GetKey(playerTurnKeyOptional)){player.transform.rotation = gameObject.transform.rotation;}}}
---
+++ Codebase/Components/Testbed/MoveTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zios;
+public class MoveTest : MonoBehaviour{
+	public KeyCode keyForward = KeyCode.W;
+	public KeyCode keyBack = KeyCode.S;
+	public ColliderController cMove;
+	private Vector3 moveVector;
+	void Start(){
+		cMove=gameObject.GetComponent<ColliderController>();}
+	void Update(){
+		if (Input.GetKeyDown(keyForward)){moveVector = new Vector3(0,0,30);
+		}if (Input.GetKeyUp(keyForward)){moveVector = new Vector3(0,0,0);}
+		if (Input.GetKeyDown(keyBack)){moveVector = new Vector3(0,0,-30);
+		}if (Input.GetKeyUp(keyBack)){moveVector = new Vector3(0,0,0);}
+		cMove.move.Add(moveVector);}}
