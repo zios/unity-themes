e423a68dadaa4b13d87578724cecb569e76a8600
ArnoldTakacs <ArnoldTakacs@mycompany.com>
2014-08-26 18:35:53 +0000

    - Cleaned up the move/input test code a bit
    git-svn-id: http://ziosproject.com/Shared@93 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

--- Codebase/Components/Testbed/MoveTest.cs
+++ Codebase/Components/Testbed/MoveTest.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Zios;
+[RequireComponent(typeof(ColliderController))]
 public class MoveTest : MonoBehaviour{
 	public KeyCode keyForward = KeyCode.W;
 	public KeyCode keyBack = KeyCode.S;
-	public ColliderController cMove;
+	public KeyCode keyLeft = KeyCode.A;
+	public KeyCode keyRight = KeyCode.D;
+	public ColliderController characterMove;
 	private Vector3 moveVector;
 	void Start(){
-		cMove=gameObject.GetComponent<ColliderController>();}
+		characterMove=gameObject.GetComponent<ColliderController>();
+	}
 	void Update(){
-		if (Input.GetKeyDown(keyForward)){moveVector = new Vector3(0,0,30);
-		}if (Input.GetKeyUp(keyForward)){moveVector = new Vector3(0,0,0);}
-		if (Input.GetKeyDown(keyBack)){moveVector = new Vector3(0,0,-30);
-		}if (Input.GetKeyUp(keyBack)){moveVector = new Vector3(0,0,0);}
-		cMove.move.Add(moveVector);}}
+		if(Input.GetKey(keyForward)){moveVector = new Vector3(0,0,7);}
+		else if(Input.GetKey(keyBack)){moveVector = new Vector3(0,0,-7);}
+		else if(Input.GetKey(keyLeft)){moveVector = new Vector3(-7,0,0);}
+		else if(Input.GetKey(keyRight)){moveVector = new Vector3(7,0,0);}
+		else{moveVector = new Vector3(0,0,0);}
+		characterMove.move.Add(moveVector);
+	}
+}
