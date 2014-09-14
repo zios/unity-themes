2ac375791525de227c3a9f06d2cf8a605cb14e49
ArnoldTakacs <ArnoldTakacs@mycompany.com>
2014-09-14 13:43:10 +0000

    - Replaced the existing Debug component (ShowFPS) from the legacy Unity GUI to the new one
    git-svn-id: http://ziosproject.com/Shared@99 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

--- Codebase/Systems/Debug/FpsGUI.cs
+++ Codebase/Systems/Debug/FpsGUI.cs
@@ -1,21 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Text;
 [AddComponentMenu("Zios/Singleton/ShowFPS")]
-[RequireComponent(typeof(OverlayText))]
 public class FpsGUI : MonoBehaviour {
 	private int frames = 0;
+	private Text element;
 	private float nextUpdate;
-	private string lastFPS;
-	private StringBuilder data = new StringBuilder();
+	public void Start(){
+		this.element = this.GetComponent<Text>();
+	}
 	public void Update(){
 		this.frames += 1;
 		if(Time.time >= this.nextUpdate){
 			this.nextUpdate = Time.time + 1;
-			this.data.Clear();
-			this.data.Append("<color=yellow>FPS</color> : ");
-			this.data.Append(this.frames);
-			OverlayText.Get("FPS").UpdateText(this.data.ToString());
+			this.element.text = this.frames.ToString();
 			this.frames = 0;
 		}
 	}
