abb780896459836cc69152af0ac330536ec7e71e
BradSmithee <BradSmithee@mycompany.com>
2014-12-24 01:35:35 +0000

    -- Modified FpsGUI to include frameTime. -- Updated Debug interface layout.
    git-svn-id: http://ziosproject.com/Shared@242 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

--- Codebase/Systems/Debug/FpsGUI.cs
+++ Codebase/Systems/Debug/FpsGUI.cs
@@ -1,38 +1,23 @@
 using UnityEngine;
-#if UNITY_4_6 || UNITY_5_0
 using UnityEngine.UI;
-#endif
 using System;
 using System.Text;
 [AddComponentMenu("Zios/Singleton/ShowFPS")]
-#if !UNITY_4_6 && !UNITY_5_0
-	[RequireComponent(typeof(OverlayText))]
-#endif
 public class FpsGUI : MonoBehaviour {
+	public Text fpsText;
+	public Text frameTimeText;
 	private int frames = 0;
+	private float frameTime;
+	private float lastUpdate;
 	private float nextUpdate;
-	#if UNITY_4_6 || UNITY_5_0
-		private Text element;
-		public void Start(){
-			this.element = this.GetComponent<Text>();
-		}
-	#else
-		private string lastFPS;
-		private StringBuilder data = new StringBuilder();
-	#endif
 	public void Update(){
 		this.frames += 1;
 		if(Time.time >= this.nextUpdate){
 			this.nextUpdate = Time.time + 1;
-			#if UNITY_4_6 || UNITY_5_0
-				this.element.text = this.frames.ToString();
-			#else
-				this.data.Clear();
-				this.data.Append("FPS : ");
-				this.data.Append(this.frames);
-				OverlayText.Get("FPS").UpdateText(this.data.ToString());
-			#endif
+			this.fpsText.text = this.frames.ToString();
+			this.frameTimeText.text = (Time.time - this.frameTime).ToString("0.0000");
 			this.frames = 0;
 		}
+		this.frameTime = Time.time;
 	}
 }
