8f7c4d7b6dec86751491f2af36bfd1c6d9d40b3e
BradSmithee <BradSmithee@mycompany.com>
2015-08-14 19:33:05 +0000

    • UpdateParticle will no longer occur in the editor unless AlwaysUpdateParticles is on.
    git-svn-id: http://ziosproject.com/Shared@363 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

--- Codebase/Components/Particle/UpdateParticle.cs
+++ Codebase/Components/Particle/UpdateParticle.cs
@@ -4,11 +4,13 @@ public class UpdateParticle : MonoBehaviour{
 	#if UNITY_EDITOR
 	public void OnWillRenderObject(){
 		if(!Application.isPlaying && Camera.current != null){
-			float range = UnityEditor.EditorPrefs.GetFloat("EditorSettings-ParticleUpdateRange");
-			Vector3 cameraPosition = Camera.current.transform.position;
-			Vector3 objectPosition = this.transform.position;
-			if(Vector3.Distance(cameraPosition,objectPosition) <= range){
-				this.GetComponent<ParticleSystem>().Simulate(Time.realtimeSinceStartup%60+10);
+			if(UnityEditor.EditorPrefs.GetBool("EditorSettings-AlwaysUpdateParticles")){
+				float range = UnityEditor.EditorPrefs.GetFloat("EditorSettings-ParticleUpdateRange");
+				Vector3 cameraPosition = Camera.current.transform.position;
+				Vector3 objectPosition = this.transform.position;
+				if(Vector3.Distance(cameraPosition,objectPosition) <= range){
+					this.GetComponent<ParticleSystem>().Simulate(Time.realtimeSinceStartup%60+10);
+				}
 			}
 		}
 	}
