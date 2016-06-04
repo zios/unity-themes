6b51dea4573dd0985a94ce1b6ddb7ec139be4377
ArnoldTakacs <ArnoldTakacs@mycompany.com>
2016-06-04 08:25:25 +0000

    git-svn-id: http://ziosproject.com/Shared@477 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

--- Codebase/Compile Theme.bat
+++ Codebase/Compile Theme.bat
@@ -5,10 +5,10 @@ IF EXIST "%programfiles(x86)%\MSBuild\15.0\Bin" GOTO USE_VS15_X86
 IF EXIST "%programfiles(x86)%\MSBuild\15.0\Bin" GOTO USE_VS15_X64
 :USE_VS14_X86
 	echo Compiling Theme using Visual Studio 2015 MSBuild (32-bit)...
-	"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ZiosThemes.sln
+	"%programfiles%\MSBuild\14.0\Bin\MSBuild.exe" ZiosThemes.sln
 :USE_VS15_X86
 	echo Compiling Theme using Visual Studio 15 MSBuild (32-bit)...
-	"%programfiles(x86)%\MSBuild\15.0\Bin\MSBuild.exe" ZiosThemes.sln
+	"%programfiles%\MSBuild\15.0\Bin\MSBuild.exe" ZiosThemes.sln
 :USE_VS14_X64
 	echo Compiling Theme using Visual Studio 2015 MSBuild (64-bit)...
 	"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ZiosThemes.sln
