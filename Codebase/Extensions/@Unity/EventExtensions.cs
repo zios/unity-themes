4e61f68ad02d412f5f461dbc16086f1301284f60
Takacs Arnold <takacsarnold@outlook.com>
2016-07-10 18:04:15 +0300

    -- Renamed fonts once more to be of Title Case folder wise rather than PascalCase -- Added missing .meta files for Unity versioning consistence when being used directly without compilation

---
+++ Codebase/Extensions/@Unity/EventExtensions.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+namespace Zios{
+	public static class EventExtension{
+		public static bool IsUseful(this Event current){
+			if(current.type == EventType.ScrollWheel){return false;}
+			if(current.type == EventType.Ignore){return false;}
+			if(current.type == EventType.Used){return false;}
+			return true;
+		}
+	}
+}
