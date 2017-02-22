58350e8aa9333d5eb3f2d60a26a13b7267ecaf88
ArnoldTakacs <ArnoldTakacs@mycompany.com>
2017-02-22 11:29:05 +0000

    -- Added compilation support for the GUISkinEditor project solution to .dll, similar to Themes -- Removed .meta attachments to already ignored theme font's .Data folder contents -- Added ZiosGUISkinEditor meta variants for its compiled .dll's (starting from 5.0.4 to 5.6.0)
    git-svn-id: http://ziosproject.com/Shared@548 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

---
+++ Codebase/.Solutions/Properties/AssemblyInfoGUISkinEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Zios GUISkin Editor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zios")]
+[assembly: AssemblyProduct("ZiosGUISkinEditor")]
+[assembly: AssemblyCopyright("Copyright © Zios 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9A8DEC36-BA9D-4780-8B37-13F015CBA49D")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("{unityversion}.{revision}")]
+[assembly: AssemblyFileVersion("{unityversion}.{revision}")]
