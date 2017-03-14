7fdb75d1e97f54c31d3e16ba94e4d29e1e667a3e
ArnoldTakacs <ArnoldTakacs@mycompany.com>
2017-03-14 13:38:56 +0000

    -- Readjusted ZiosThemes and ZiosGUISkinEditor projects to have the proper source code files to build the target .dll's
    git-svn-id: http://ziosproject.com/Shared@571 ee7e34eb-f0ec-4b85-9eae-efee4cd27b06

--- Codebase/Containers/Container.cs
+++ Codebase/Containers/Container.cs
@@ -91,7 +91,9 @@ namespace Zios.Containers{
 	public class Hierarchy<KeyA,KeyB,KeyC,Value> : Dictionary<KeyA,Dictionary<KeyB,Dictionary<KeyC,Value>>>{public Hierarchy():base(){}}
 	public class Hierarchy<KeyA,KeyB,KeyC,KeyD,Value> : Dictionary<KeyA,Dictionary<KeyB,Dictionary<KeyC,Dictionary<KeyD,Value>>>>{public Hierarchy():base(){}}
 	public class Hierarchy<KeyA,KeyB,KeyC,KeyD,KeyE,Value> : Dictionary<KeyA,Dictionary<KeyB,Dictionary<KeyC,Dictionary<KeyD,Dictionary<KeyE,Value>>>>>{public Hierarchy():base(){}}
+	#if ENABLE_MONO
 	public class Hierarchy<KeyA,KeyB,KeyC,KeyD,KeyE,KeyF,Value> : Dictionary<KeyA,Dictionary<KeyB,Dictionary<KeyC,Dictionary<KeyD,Dictionary<KeyE,Dictionary<KeyF,Value>>>>>>{public Hierarchy():base(){}}
 	public class Hierarchy<KeyA,KeyB,KeyC,KeyD,KeyE,KeyF,KeyG,Value> : Dictionary<KeyA,Dictionary<KeyB,Dictionary<KeyC,Dictionary<KeyD,Dictionary<KeyE,Dictionary<KeyF,Dictionary<KeyG,Value>>>>>>>{public Hierarchy():base(){}}
 	public class Hierarchy<KeyA,KeyB,KeyC,KeyD,KeyE,KeyF,KeyG,KeyH,Value> : Dictionary<KeyA,Dictionary<KeyB,Dictionary<KeyC,Dictionary<KeyD,Dictionary<KeyE,Dictionary<KeyF,Dictionary<KeyG,Dictionary<KeyH,Value>>>>>>>>{public Hierarchy():base(){}}
+	#endif
 }
